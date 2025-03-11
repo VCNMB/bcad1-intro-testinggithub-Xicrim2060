d92d06b7d4402878c6a975a717377dbec0dd5855
Xicrim2060 <Luchusizani26@gmail.com>
2025-03-11 10:38:58 +0200

    added new class

---
+++ prjClasses/src/main/java/com/mycompany/prjclasses/NewClass.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.prjclasses;
+
+/**
+ *
+ * @author lab_services_student
+ */
+public class NewClass {
+    
+    public String strSecondClassVar = "Testing";
+    
+   public void helloClass()
+   {
+       System.out.println("Hello from the 2nd class");
+   }        
+}
--- prjClasses/src/main/java/com/mycompany/prjclasses/PrjClasses.java
+++ prjClasses/src/main/java/com/mycompany/prjclasses/PrjClasses.java
@@ -12,5 +12,11 @@ public class PrjClasses {
 
     public static void main(String[] args) {
         System.out.println("Hello World I just did github and love jay now");
+        
+        NewClass nc = new NewClass();
+        
+        nc.helloClass();
+        
+        System.out.println(nc.strSecondClassVar);
     }
 }
